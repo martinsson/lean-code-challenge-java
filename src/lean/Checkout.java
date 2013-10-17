8e3d94dd7b62478e161c86c52225a2aa223704e0
Nicolas Terray <nicolas.terray@gmail.com>
2013-10-17 23:11:51 +0200

    Remove usage of Systen.console()
    The System.console() was null in eclipse' console.
    
    By the way, add a basic prompt.

--- src/lean/Checkout.java
+++ src/lean/Checkout.java
@@ -2,6 +2,7 @@ package lean;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 public class Checkout {
@@ -9,8 +10,8 @@ public class Checkout {
      * java -cp ./bin/ lean/Checkout
      */
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(System.console().reader());
-        PrintWriter writer = System.console().writer();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter writer = new PrintWriter(System.out, true);
         Cashier cashier = new Cashier();
         cashier.checkout(reader, writer);
     }
